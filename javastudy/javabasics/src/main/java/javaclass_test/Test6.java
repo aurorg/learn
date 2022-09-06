99fcad523f8c28f1e4cc680f2802d69866a29914
aurorg <2519652258@qq.com>
2022-09-06 12:35:43 +0800

    1

--- javastudy/javabasics/src/main/java/javaclass_test/Test6.java
+++ javastudy/javabasics/src/main/java/javaclass_test/Test6.java
@@ -4,47 +4,47 @@ import java.util.Scanner;
 
 public class Test6 {
     public static void main(String[] args) {
-        Scanner sc =new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-        double a=sc.nextDouble();
-        double b=sc.nextDouble();
-        double c=sc.nextDouble();
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        double c = sc.nextDouble();
 
         double delta;
         delta = b * b - 4 * a * c;
         double x1, x2;
 
-        if(a==0 && b==0 & c==0){
+        if (a == 0 && b == 0 & c == 0) {
             System.out.println("Zero Equation");
-        }
-        if(a==0 && b==0 && c!=0){
+        } else if (a == 0 && b == 0 && c != 0) {
             System.out.println("Not An Equation");
-        }
-        if(delta==0){
-            x1=b/2.0/a;
-            x2=b/2.0/a;
+        } else if (a == 0 && b != 0) {
+            x1 = -c / b;
+            if (x1 == 0) {
+                x1 = 0;
+            }
+            System.out.printf("%.2f", x1);
+        } else if (delta == 0) {
+            x1 = -b / 2.0 / a;
 
-            System.out.printf("%.2d",x1);
-        }
-        if(delta>0){
+            System.out.printf("%.2f", x1);
+        } else if (delta > 0) {
             x1 = (-b + Math.sqrt(delta)) / 2.0 / a;
             x2 = (-b - Math.sqrt(delta)) / 2.0 / a;
-
-            if(x1<x2){
-                double t=x1;
-                x1=x2;
-                x2=t;
-            }
-
-            System.out.printf("%.2d",x1);
-            System.out.printf("%.2d",x2);
-        }
-        if(delta<0){
+            System.out.printf("%.2f\n",x1);
+            System.out.printf("%.2f",x2);
+        } else if (delta < 0) {
             double s, x;
             s = -b / 2.0 / a;
             x = Math.sqrt(-delta) / 2.0 / a;
-            System.out.println(s + "+" +x + "i");
-            System.out.println(s + "-" + x +"i");
+            x=Math.abs(x);
+            if(s==0){
+                s=0;
+            }
+
+            System.out.printf("%.2f+%.2fi\n", s, x);
+            System.out.printf("%.2f-%.2fi\n", s, x);
+
         }
 
     }
---
+++ javastudy/javabasics/src/main/java/javaclass_test/report1/Test3.java
@@ -0,0 +1,7 @@
+package javaclass_test.report1;
+
+public class Test3 {
+    public static void main(String[] args) {
+        
+    }
+}
