d8265d71dd9ffa62c9f32754803aa82750fa7f36
aurorg <2519652258@qq.com>
2022-09-12 20:46:44 +0800

    1

--- javastudy/javabasics/src/main/java/javaclass_test/two/Test.java
+++ javastudy/javabasics/src/main/java/javaclass_test/two/Test.java
@@ -3,10 +3,8 @@ package javaclass_test.two;
 
 public class Test{
     static int arr[] = new int[10];
-
-
     public static void main(String a[]){
-        
+
         System.out.println(arr[1]);
     }
 }
