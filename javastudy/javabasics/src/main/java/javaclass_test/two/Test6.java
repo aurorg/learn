b1b1a8e9ecf1a2dcec8b23ae6aaf4645b1a9c67c
aurorg <2519652258@qq.com>
2022-09-12 13:42:08 +0800

    1

--- javastudy/javabasics/src/main/java/javaclass_test/two/Test6.java
+++ javastudy/javabasics/src/main/java/javaclass_test/two/Test6.java
@@ -6,8 +6,10 @@ public class Test6 {
     public static void main(String[] args) {
         Scanner input =new Scanner(System.in);
 
+        
         int n=input.nextInt();
         int x=input.nextInt();
+
 
         int[] a=new int[n];
         for(int i=0;i<n;i++){
