6644d9fb144e3e7025299286930b77136c16ceeb
aurorg <2519652258@qq.com>
2022-09-12 20:44:23 +0800

    1

--- javastudy/javabasics/src/main/java/javaclass_test/report1/a1/Test6.java
+++ javastudy/javabasics/src/main/java/javaclass_test/report1/a1/Test6.java
@@ -3,6 +3,7 @@ package javaclass_test.report1.a1;
 import java.util.Scanner;
 
 public class Test6 {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
--- javastudy/javabasics/src/main/java/javaclass_test/two/Test.java
+++ javastudy/javabasics/src/main/java/javaclass_test/two/Test.java
@@ -4,7 +4,9 @@ package javaclass_test.two;
 public class Test{
     static int arr[] = new int[10];
 
+
     public static void main(String a[]){
+        
         System.out.println(arr[1]);
     }
 }
