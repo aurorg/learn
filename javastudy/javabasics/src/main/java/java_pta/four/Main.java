311e9f0b15f3cf906629bb2e9641e5f8f6b134f4
aurorg <2519652258@qq.com>
2022-11-16 22:03:00 +0800

    1

---
+++ javastudy/javabasics/src/main/java/java_pta/four/Main.java
@@ -0,0 +1,16 @@
+//package java_pta.four;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        Person p=new Person("pp”);
+//        Student s=new Student();
+//        Employee e=new Employee();
+//        Faculty f=new Faculty();
+//        Staff st=new Staff();
+//        System.out.println(p.toString());
+//        System.out.println(s.toString());
+//        System.out.println(e.toString());
+//        System.out.println(f.toString());
+//        System.out.println(st.toString());
+//    }
+//}
---
+++ javastudy/javabasics/src/main/java/java_pta/four/Person.java
@@ -0,0 +1,48 @@
+//package java_pta.four;
+//
+//public class Person {
+//    String name;
+//    String address;
+//    int number;
+//    String emile;
+//    public Person(String name){
+//        this.name=name;
+//    }
+//    public String toString(){
+//        return " class : "+getClass()+" name :" +name;
+//    }
+//}
+//class Student extends Person{
+//    String grade;
+//    public String toString(){
+//        return " class : "+getClass()+" name :" +name;
+//    }
+//}
+//class Employee extends Person{
+//    String office;
+//    int salary;
+//    MyDate spdate=new MyDate();
+//    public String toString(){
+//        return " class : "+getClass()+" name :" +name;
+//    }
+//}
+//class MyDate{
+//    int year;
+//    int month;
+//    int day;
+//
+//}
+//
+//class Faculty extends Employee{
+//    MyDate bgdate=new MyDate();
+//    String jibie;
+//    public String toString(){
+//        return " class : "+getClass()+" name :" +name;
+//    }
+//}
+//class Staff extends Employee{
+//    String designation;
+//    public String toString(){
+//        return " class : "+getClass()+" name :" +name;
+//    }
+//}
