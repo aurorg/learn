06d0154281987aa4748c999fa1bece8e80db3ac0
aurorg <2519652258@qq.com>
2022-11-27 15:15:42 +0800

    1

--- javastudy/demo1/src/main/java/com/example/demo1/test3.java
+++ javastudy/demo1/src/main/java/com/example/demo1/test3.java
@@ -3,6 +3,15 @@ package com.example.demo1;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
 public class test3 extends Application {
 
     public static void main(String[] args) {
@@ -11,6 +20,14 @@ public class test3 extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+        Text text = new Text("A");
+        StackPane pane = new StackPane(text);
 
+        Scene scene = new Scene(pane, 300, 200);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Exercise15_10");
+        primaryStage.show();
+
+        scene.setOnKeyPressed(event -> text.setText(event.getCode() == KeyCode.ENTER ? "" : text.getText() + event.getText()));
     }
 }
