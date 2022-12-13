f1e6de1e10cb8dc9a58cc2fbd668a43e9b64db59
aurorg <2519652258@qq.com>
2022-12-13 15:35:29 +0800

    1

--- javastudy/javabasics/src/main/java/java_pta/report11/Test4.java
+++ javastudy/javabasics/src/main/java/java_pta/report11/Test4.java
@@ -1,4 +1,44 @@
 package java_pta.report11;
 
+import java.util.*;
+import java.io.*;
+
 public class Test4 {
+    public static void main(String[] args) throws Exception {
+        // Check number of arguments passed
+        if (args.length != 1) {
+            System.out.println("yisdhgfbsdrhfgbysdhj");
+            System.exit(1);
+        }
+
+        // Check if file exists
+        File textFile = new File(args[0]);
+        if (!textFile.exists()) {
+            System.out.println("The file " + args[0] + " does not exist.");
+            System.exit(2);
+        }
+
+        // Create a list of string
+        List<String> list = new ArrayList<>();
+
+        try (
+                // Create input file
+                Scanner input = new Scanner(textFile);
+        ) {
+            while (input.hasNext()) {
+                String[] array = input.nextLine().split(" ");
+                for (int i = 0; i < array.length; i++) {
+                    if (array[i].length() > 0 &&
+                            Character.isLetter(array[i].charAt(0))) {
+                        list.add(array[i]);
+                    }
+                }
+            }
+        }
+        // Sort the list in ascending alphabetical order
+        Collections.sort(list);
+
+        // Display the list
+        System.out.println(list);
+    }
 }
---
+++ javastudy/javabasics/src/main/java/java_pta/report11/Test7.java
@@ -0,0 +1,21 @@
+//package java_pta.report11;
+//
+//
+//import java.util.Comparator;
+//
+//public class Test7 {
+//    public class GeometricObjectComparator
+//            implements Comparator<GeometricObject>, java.io.Serializable {
+//        public int compare(GeometricObject o1, GeometricObject o2) {
+//            double area1 = o1.getArea();
+//            double area2 = o2.getArea();
+//
+//            if (area1 < area2)
+//                return -1;
+//            else if (area1 == area2)
+//                return 0;
+//            else
+//                return 1;
+//        }
+//    }
+//}
